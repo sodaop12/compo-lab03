9ba38362c482a1b5e56bc4d0d3b283447effd1e5
sodaop <saiphirun_p@cmu.ac.th>
2022-07-20 23:43:02 +0700

    4.12

---
+++ src/components/EventCardInfo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="event-card-info">
+    <h4>{{ event.category }}</h4>
+    <h4>{{ event.organizer }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCardInfo',
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-card-info {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  text-align: right;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
